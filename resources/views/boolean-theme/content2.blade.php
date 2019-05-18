737e7d3fca7ae92a31561dcb0591b2de8a426d52
Federico <federico.dente92@gmail.com>
2019-05-18 15:43:17 +0200

    Quasi fatto manca robetta nel css

--- resources/views/boolean-theme/content2.blade.php
+++ resources/views/boolean-theme/content2.blade.php
@@ -1,35 +1,43 @@
+<div class="faq-head p-5">
+
+  <h1>Come si diventa sviluppatore</h1>
+  <p>Le nostre risposte a tutte le tue domande. Non trovi la risposta che stai cercando? Contattaci e saremo felici di darti una mano.</p>
+</div>
+
 <div class="faq d-flex w-100">
-  <div class="faqSx col-md-6">
+  <div class="faqSx col-md-6 p-5">
 
-  <h2>corso</h2>
-  <h4></h4> <span>+</span>
-  <p></p>
+  <h2 class="font-weight-bold">Prima del corso</h2>
+  <h4>Chi può fare il corso Boolean?</h4><span>+</span>
+  <p>Chiunque abbia una forte curiosità verso il mondo della programmazione e voglia di imparare a fare il mestiere di sviluppatore. Età e curriculum non sono una discriminante: abbiamo avuto studenti dai 18 ai 45 anni, dal ragazzo senza esperienza lavorativa al professionista affermato che vuole cambiare settore. Se non hai mai programmato o se hai già seguito qualche corso ma senti di non aver approfondito abbastanza per essere autonomo, Boolean è il corso che fa per te. </p>
 
-  <h4></h4> <span>+</span>
-  <p></p>
+  <h4>Che competenze devo avere per iniziare il corso?</h4><span>+</span>
+  <p>Il corso parte da zero quindi non serve alcuna competenza di programmazione; è necessario però superare il processo di selezione.</p>
 
-  <h4></h4> <span>+</span>
-  <p></p>
+  <h4>Questo corso è una valida alternativa alla laurea?</h4><span>+</span>
+  <p>Lo è sicuramente in quanto metodo alternativo per accedere al mondo del lavoro: le aziende che cercano sviluppatori sanno che una laurea non è garanzia di saper programmare e sono più che disposte a valutare candidati che non hanno frequentato una università. Non ci sono percorsi di carriera nel mondo della programmazione che ti sono preclusi se non hai una laurea.</p>
 
-  <h4></h4> <span>+</span>
-  <p></p>
+  <h4>C'è un processo di selezione?</h4><span>+</span>
+  <p>Si, Boolean è un corso intensivo che richiede impegno costante, dedizione e una attitudine al problem solving. Per assicurarci che ogni studente abbia concrete possibilità di diventare uno sviluppatore e iniziare una nuova carriera, selezioniamo gli studenti tramite un test di logica a risposta multipla e un colloquio motivazionale. Contattaci per iniziare il tuo percorso o saperne di più!</p>
 
   </div>
 
-  <div class="faqDx col-md-6 ">
+  <div class="faqDx col-md-6 text-white p-5">
 
-  <h2>corso</h2>
-  <h4></h4> <span>+</span>
-  <p></p>
+  <h2 class="font-weight-bold">Dopo il corso</h2>
+  <h4>Ho la certezza di essere assunto?</h4><span>+</span>
+  <p>Dipende tutto da te! Lo sviluppatore web è uno dei mestieri più ricercati in Italia come dimostrano numerosi studi di settore e Boolean fornisce tutte le competenze richieste dalle aziende per iniziare a fare questo lavoro. Oltre alle conoscenze di programmazione, il nostro team ti presenterà alle nostre aziende partner che assumono in tutta Italia e ti aiuterà a prepararti al meglio per i colloqui!</p>
 
-  <h4></h4> <span>+</span>
-  <p></p>
+  <h4>Terminato il corso sarò supportato nella ricerca del lavoro?</h4><span>+</span>
+  <p>Terminato il corso sarò supportato nella ricerca del lavoro?</p>
 
-  <h4></h4> <span>+</span>
-  <p></p>
+  <h4>Quanti dei vostri ex allievi hanno ricevuto offerte di lavoro?</h4><span>+</span>
+  <p>Il 100%. Tutti gli studenti che hanno terminato il corso hanno ricevuto una o più offerte di lavoro. E' un mestiere veramente molto ricercato, il nostro corso fornisce una base solida e le aziende non si fanno scappare la possibilità di assumerli.
 
-  <h4></h4> <span>+</span>
-  <p></p>
+</p>
+
+<h4>Posso fare lo sviluppatore freelance al termine del corso?</h4><span>+</span>
+  <p>E' una possibilità ma sconsigliamo di intraprendere questa strada subito dopo il corso: per essere sviluppatori freelance l'esperienza è fondamentale e difficilmente sono sufficienti 6 mesi. Se il tuo obiettivo è lavorare seguendo i tuoi ritmi, la strada è quella giusta: molte aziende si stanno aprendo al work from home e, con un po' di esperienza in più, diventerai un ottimo sviluppatore freelance!</p>
 
   </div>
 </div>
